8b62c17e428151428873e8848f0c313c9d4950ac
Dung <dungtrinh0702@gmail.coom>
2020-05-26 21:18:10 +0700

    mapping search

--- src/main/java/com/codegym/casestudy/controller/ContactController.java
+++ src/main/java/com/codegym/casestudy/controller/ContactController.java
@@ -4,7 +4,9 @@ import com.codegym.casestudy.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ContactController {
@@ -14,6 +16,16 @@ public class ContactController {
     @GetMapping("/contact")
     public String list(Model model) {
         model.addAttribute("contacts", contactService.findAll());
+        return "list";
+    }
+
+    @GetMapping("/contact/search")
+    public String search(@RequestParam("term") String term, Model model) {
+        if (StringUtils.isEmpty(term)) {
+            return "redirect:/contact";
+        }
+
+        model.addAttribute("contacts", contactService.search(term));
         return "list";
     }
 }
